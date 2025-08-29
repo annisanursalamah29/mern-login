5614ed38da8a28eaa012b49f656e80c7c6a85e39
annisanursalamah29@gmail.com <annisanursalamah29@gmail.com>
2025-08-29 19:06:10 +0700

    kedua

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,19 +11,16 @@ function App() {
     <Router>
       <div>
         <nav>
-           <Link to="/">Home</Link> | 
-           <Link to="/login">Login</Link> | 
-           <Link to="/register">Register</Link> | 
-            <Link to="/crud">CRUD</Link> |
-            <Link to="/users">User List</Link>
+          <Link to="/mern-login">Home</Link> |<Link to="/login">Login</Link> |
+          <Link to="/register">Register</Link> |<Link to="/crud">CRUD</Link> |
+          <Link to="/users">User List</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/mern-login" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/crud" element={<CrudPage />} /> 
-          <Route path="/users" element={<UserList />} />  
-
+          <Route path="/crud" element={<CrudPage />} />
+          <Route path="/users" element={<UserList />} />
         </Routes>
       </div>
     </Router>
